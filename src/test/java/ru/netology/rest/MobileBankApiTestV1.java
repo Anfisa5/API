7f1f1615deec56dec7131885aed46d9d6ab13924
Anfisa Golovanova <anfisa.faa@gmail.com>
2023-01-15 18:43:31 +0700

    update MobileBankApiTestV1 and modified accounts.schema.json

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -2,12 +2,15 @@ package ru.netology.rest;
 
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Headers;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 class MobileBankApiTestV1 {
+
     @Test
     void shouldReturnDemoAccounts() {
         // Given - When - Then
@@ -19,11 +22,78 @@ class MobileBankApiTestV1 {
                 .get("/demo/accounts")
                 // Проверки
                 .then()
-                .statusCode(200)
-                .header("Content-Type", "application/json; charset=UTF-8")
-                .contentType(ContentType.JSON)
-                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+                .statusCode(200);
+    }
 
+    @Test
+    void shouldContentTypeIsJSON() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .header("Content-Type", "application/json; charset=UTF-8")
+                .contentType(ContentType.JSON);
+    }
+
+    @Test
+    void shouldSizeIs3() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .body("", hasSize(3));
+    }
+
+    @Test
+    void shouldJsonSchema() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+    }
+
+    @Test
+    void shouldId2IsCurrencyUSD() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .body("[1].currency", equalTo("USD"));
+    }
+
+    @Test
+    void shouldAllBalanceIsPositive() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .body("every { it.balance >= 0}", is(true));
     }
 
 }
